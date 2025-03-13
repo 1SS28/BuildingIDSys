ecbf8115c02005b71fabac418eee936876a73308
AarushBatarachi <sanjeyselvendra8@gmail.com>
2025-03-13 00:54:46 -0400

    Create BuildingIDSys.java
    boiler plate code to test commit

---
+++ BuildingIDSys.java
@@ -0,0 +1,16 @@
+//First iteration
+
+import java.util.*;
+
+public class BuildingIDSys{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+
+        sc.close();
+    }
+}
+
